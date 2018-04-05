9c029a317c5cdbecb31f8e429950d551368b80cd
Valera Vitkus <1571312@gmail.com>
2018-04-05 22:17:07 +0300

    add methods to message service

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -2,6 +2,14 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class MessageService {
+  messages: string[] = [];
+
+  add(message: string) {
+    this.messages.push(message);
+  }
+  clear() {
+    this.messages = [];
+  }
 
   constructor() { }
 
