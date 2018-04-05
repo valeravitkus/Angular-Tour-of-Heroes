6ec2c47561a7a914726ee679d34c96af224966c0
Valera Vitkus <1571312@gmail.com>
2018-04-05 21:43:23 +0300

    Inject the HeroService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
 
 
 @NgModule({
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+
 import { HEROES } from './../mock-heroes';
 import { Hero } from './../hero';
+
+import { HeroService } from './../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,14 +12,14 @@ import { Hero } from './../hero';
 })
 export class HeroesComponent implements OnInit {
 
-heroes = HEROES;
+heroes = Hero[];
 selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
   }
