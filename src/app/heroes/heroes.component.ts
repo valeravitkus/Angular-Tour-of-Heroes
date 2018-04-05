fef337560200eea1d5b3fef2c98af3e2055c1a84
Valera Vitkus <1571312@gmail.com>
2018-04-05 21:57:56 +0300

    Observable HeroService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,17 +12,16 @@ import { HeroService } from './../hero.service';
 })
 export class HeroesComponent implements OnInit {
 
-heroes = Hero[];
 selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
   getHeroes(): void {
     this.heroes = this.heroService.getHeroes();
